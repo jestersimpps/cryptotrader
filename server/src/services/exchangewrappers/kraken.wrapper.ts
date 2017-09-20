4e5116dad8a21975b82caee41bb0198aceb2e133
jestersimpps <jestersimpps@gmail.com>
2017-09-20 23:30:24 +0200

    200 check

--- server/src/services/exchangewrappers/kraken.wrapper.ts
+++ server/src/services/exchangewrappers/kraken.wrapper.ts
@@ -31,27 +31,32 @@ export class KrakenWrapper extends ApiWrapper {
                 const tickerUrl = this.composeUrl(`Ticker?pair=${pairs.join(',')}`);
                 return RxHttpRequest.get(tickerUrl, {})
                     .map((data) => {
-                        const tickers: TickerDto[] = [];
-                        const body = data.response.body.json().result;
-                        Object.keys(body).forEach((key, index) => {
-                            tickers.push({
-                                id: index,
-                                exchange: this.exchange,
-                                symbol: key,
-                                last: body[key].c[0],
-                                lowestAsk: body[key].a[0],
-                                highestBid: body[key].b[0],
-                                percentChange: (body[key].o - body[key].c[0]) / body[key].o,
-                                base: key.split(`_`)[0],
-                                quote: key.split(`_`)[1],
-                                baseVolume: body[key].v[0],
-                                quoteVolume: body[key].v[0],
-                                isFrozen: false,
-                                high24hr: body[key].h[0],
-                                low24hr: body[key].l[0]
+                        if (data.response.statusCode === 200) {
+                            const tickers: TickerDto[] = [];
+                            const body = data.response.body.json().result;
+                            Object.keys(body).forEach((key, index) => {
+                                tickers.push({
+                                    id: index,
+                                    exchange: this.exchange,
+                                    symbol: key,
+                                    last: body[key].c[0],
+                                    lowestAsk: body[key].a[0],
+                                    highestBid: body[key].b[0],
+                                    percentChange: (body[key].o - body[key].c[0]) / body[key].o,
+                                    base: key.split(`_`)[0],
+                                    quote: key.split(`_`)[1],
+                                    baseVolume: body[key].v[0],
+                                    quoteVolume: body[key].v[0],
+                                    isFrozen: false,
+                                    high24hr: body[key].h[0],
+                                    low24hr: body[key].l[0]
+                                });
                             });
-                        });
-                        return tickers;
+                            return tickers;
+                        } else {
+                            // TODO: error handling
+                            return [];
+                        }
                     })
             });
     }
