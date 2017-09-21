e2a453ff51f92541501741d3ec17c8f68baf7aea
jestersimpps <jestersimpps@gmail.com>
2017-09-21 18:19:42 +0200

    added redis

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 import * as bodyParser from 'body-parser';
+import * as redis from 'redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
